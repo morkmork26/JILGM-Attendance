eedfaa2ed03ca2a4bdefadde9428197f6144cd69
Mark Lester Burgos <marklester.burgos@analog.com>
2026-08-23 21:19:44 +0800

    Auto-set outreach default per device based on last used value

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jilgm-v72';
+const CACHE='jilgm-v73';
 const CORE_ASSETS=['./','/index.html','./logo.jpg','./icon-192.png','./icon-512.png'];
 const CDN_ASSETS=['https://unpkg.com/html5-qrcode@2.3.8/html5-qrcode.min.js','https://cdn.jsdelivr.net/npm/qrcodejs@1.0.0/qrcode.min.js'];
 self.addEventListener('install',e=>{
